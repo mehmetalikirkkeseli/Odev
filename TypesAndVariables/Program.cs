6405d61a65579122cb70e4dd0d3ebcde71aa4b05
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-10-23 18:24:01 +0300

    commit19

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -20,11 +20,14 @@ namespace TypesAndVariables
             short number3 = 32767;
             int number1 = 2147483647;
             long number2 = 9223372036854775807;  //long karakter sınırı 19 dur.
+            var number7 = 10;
+            number7 = 'A';
             Console.WriteLine("Number1 is {0}",number1);
             Console.WriteLine("Number2 is {0}", number2);
             Console.WriteLine("Number3 is {0}", number3);
             Console.WriteLine("Number4 is {0}", number4);
             Console.WriteLine("Number5 is {0}", number5);
+            Console.WriteLine("Number7 is {0}", number7);
             Console.WriteLine("Character is : {0}",(int)character);
             Console.WriteLine((int)Days.Friday);
             Console.ReadLine();
