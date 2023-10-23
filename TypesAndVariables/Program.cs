6e30caf5960aefb468ebd6180664c595a3ae147a
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-10-23 18:13:06 +0300

    commit16

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -26,7 +26,12 @@ namespace TypesAndVariables
             Console.WriteLine("Number4 is {0}", number4);
             Console.WriteLine("Number5 is {0}", number5);
             Console.WriteLine("Character is : {0}",(int)character);
+            Console.WriteLine(Days.Friday);
             Console.ReadLine();
         }
+    }
+    enum Days
+    {
+        Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday
     }
 }
