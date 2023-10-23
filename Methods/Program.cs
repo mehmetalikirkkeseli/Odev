9260de83df469e4d2edc3c0bba0a3f92b63da4a5
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-10-23 20:02:45 +0300

    commit4

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -14,6 +14,7 @@ namespace Methods
             Add();
             Add();
             Add();
+            Add2(20,30);
             Console.ReadLine();
         }
 
@@ -21,6 +22,11 @@ namespace Methods
         static void Add()
         {
             Console.WriteLine("Added!!!");
+        }
+        static int Add2(int number1, int number2)
+        {
+            var result = number1 + number2;
+            return result;
         }
     }
 }
