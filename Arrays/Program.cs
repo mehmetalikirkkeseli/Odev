ed3f50fc44b10276e009521f52fdd41df55017b3
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-10-23 21:09:58 +0300

    commit5

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -14,6 +14,7 @@ namespace Arrays
             string[] students = new string[3] { "Engin", "Derin", "Salih" };
 
             string[] students2 = { "Engin", "Derin", "Salih" };
+            students2[3] = "Ahmet";
             
             foreach (var student in students2)
             {
