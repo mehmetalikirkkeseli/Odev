1cdf37bde121a3a6eeac6aa65cb8e3d00da8bf61
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-10-23 20:43:57 +0300

    commit14

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -25,6 +25,8 @@ namespace Methods
 
             Console.WriteLine(Mutiply(2,4));
             Console.WriteLine(Mutiply2(2, 4, 5));
+
+            Console.WriteLine(Add4(1,2,3,4,5,6));
             Console.ReadLine();
         }
 
@@ -55,5 +57,11 @@ namespace Methods
         {
             return number1 * number2 * number3;
         }
+        //params keyword
+        static int Add4(params int[] numbers)
+        {
+            return numbers.Sum();
+        }
+
     }
 }
