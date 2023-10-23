885276b593cf7dbcfa249fc1ea131d5388363f7e
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-10-23 20:58:45 +0300

    commit1

---
+++ Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        //Diziler
+        static void Main(string[] args)
+        {
+            string[] students = new string[3];
+            students[0] = "Engin";
+            students[1] = "Derin";
+            students[2] = "Salih";
+
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
