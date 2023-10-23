5012d4af78e365488455925c66b339cf9afebbdd
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-10-23 19:25:31 +0300

    commit5

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -10,14 +10,32 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            var number = 30;
-            if (number == 10)
+            var number = 20;
+            //if (number == 10)
+            //{
+            //    Console.WriteLine("Number is 10");
+            //}
+            //else if (number == 20)
+            //{
+            //    Console.WriteLine("Number is 20");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Number is not 10 or 20");
+            //}
+
+            //switch ifadesi
+            switch (number)
             {
-                Console.WriteLine("Number is 10");
-            }
-            else if (number == 20)
-            {
-                Console.WriteLine("Number is 20");
+                case 10:
+                    Console.WriteLine("Number is 10");
+                    break;
+                case 20:
+                    Console.WriteLine("Number is 20");
+                    break;
+                default:
+                    Console.WriteLine("Number is not 10 or 20");
+                    break;
             }
             Console.ReadLine();
         }
