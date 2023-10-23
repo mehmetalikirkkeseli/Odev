1077bc2b4b73634dbbf4454dbb83b24f4d81f485
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-10-23 21:21:25 +0300

    commit6

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -14,14 +14,31 @@ namespace Arrays
             string[] students = new string[3] { "Engin", "Derin", "Salih" };
 
             string[] students2 = { "Engin", "Derin", "Salih" };
-            students2[3] = "Ahmet";
-            
-            foreach (var student in students2)
+            //students2[3] = "Ahmet";
+
+            //foreach (var student in students2)
+            //{
+            //    Console.WriteLine(student);
+            //}
+
+            //Çok boyutlu diziler
+            string[,] regions = new string[5, 3]
             {
-                Console.WriteLine(student);
+                {"İstanbul","İzmit","Balıkesir"},
+                {"Ankara","Konya","Kırıkkale"},
+                {"Antalya","Adana","Mersin"},
+                {"Rize","Trabzon","Samsun"},
+                {"İzmir","Muğla","Manisa"},
+            };
+            //GetUpperBound en üst değeri alır.
+            for (int i = 0; i < regions.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j < regions.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(regions[i,j]);
+                }
             }
 
-            Console.WriteLine();
             Console.ReadLine();
         }
     }
