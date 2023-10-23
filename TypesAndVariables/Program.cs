65ea98c5e13fa4a330d1dfaa1d53248a57d5b1b8
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-10-23 17:46:04 +0300

    commit9

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -12,13 +12,14 @@ namespace TypesAndVariables
         {
             //Value Types
             //Console.WriteLine("Hello World!");
+            byte number4 = 255;  //0-255 arası veri tutar.
             short number3 = 32767;
-
             int number1 = 2147483647;
             long number2 = 9223372036854775807;  //long karakter sınırı 19 dur.
             Console.WriteLine("Number1 is {0}",number1);
             Console.WriteLine("Number2 is {0}", number2);
             Console.WriteLine("Number3 is {0}", number3);
+            Console.WriteLine("Number4 is {0}", number4);
             Console.ReadLine();
         }
     }
