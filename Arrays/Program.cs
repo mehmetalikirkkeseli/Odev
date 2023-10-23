940df0525e935563ed4a8a0e212a8c33dbab318c
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-10-23 21:05:25 +0300

    commit4

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -11,10 +11,7 @@ namespace Arrays
         //Diziler
         static void Main(string[] args)
         {
-            string[] students = new string[3];
-            students[0] = "Engin";
-            students[1] = "Derin";
-            students[2] = "Salih";
+            string[] students = new string[3] { "Engin", "Derin", "Salih" };
 
             string[] students2 = { "Engin", "Derin", "Salih" };
             
