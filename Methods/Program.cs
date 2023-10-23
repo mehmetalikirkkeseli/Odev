37ba0ab1e3cd6aef3639f10479972ff3bcbdc764
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-10-23 19:42:14 +0300

    commit1

---
+++ Methods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Add();
+            Console.ReadLine();
+        }
+
+        //Metotlar
+        static void Add()
+        {
+            Console.WriteLine("Added");
+        }
+    }
+}
