0aefe3bd3a58522f78091f45c5db45a3ec567182
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-10-24 14:32:27 +0300

    commit1

---
+++ Strings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    internal class Program
+    {
+        //String ifadeler
+        static void Main(string[] args)
+        {
+            string city = "Ankara";
+            Console.WriteLine(city[0]);
+            Console.ReadLine();
+        }
+    }
+}
