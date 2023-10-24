fa6b59af7e4b3565c7eaa60fd197830195433e8f
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-10-24 14:27:24 +0300

    commit17

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -20,9 +20,31 @@ namespace Loops
             //DoWhileLoop();
             //Foreach ifadesi metoda çevrilmiştir.
             //ForEachLoop();
+            if (IsPrimeNumber(6))
+            {
+                Console.WriteLine("This is a prime number");
+            }
+            else
+            {
+                Console.WriteLine("This is not a prime number");
+            }
             Console.ReadLine();
         }
 
+        //Asal sayı için metod
+        private static bool IsPrimeNumber(int number)
+        {
+            bool result = true;
+            for (int i = 2; i < number-1; i++)
+            {
+                if (number%i==0)
+                {
+                    result = false;
+                    i = number;
+                }
+            }
+            return result;
+        }
         private static void ForEachLoop()
         {
             string[] students = new string[3] { "Engin", "Derin", "Salih" };
