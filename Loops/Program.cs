6e0fd1440e2674a1821042adea470e9e498b4d8a
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-10-24 13:36:46 +0300

    commit1

---
+++ Loops/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        //Döngüler
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
