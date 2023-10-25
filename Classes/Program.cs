1d29ae7598c05020cc8d362901c42bb7895e8580
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-10-25 17:33:54 +0300

    commit4

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -12,6 +12,8 @@ namespace Classes
         static void Main(string[] args)
         {
             //Class çağırma
+            //CustomerManager class'ı ayrı oluşturulmuştur.
+            //ProductManager class'ı ayrı oluşturulmuştur.
             CustomerManager customerManager = new CustomerManager();
             customerManager.Add();
             customerManager.Update();
@@ -22,28 +24,6 @@ namespace Classes
 
             Console.ReadLine();
         }
-
-        class CustomerManager
-        {
-            public void Add()
-            {
-                Console.WriteLine("Customer Added!");
-            }
-            public void Update()
-            {
-                Console.WriteLine("Customer Updated!");
-            }
-        }
-        class ProductManager
-        {
-            public void Add()
-            {
-                Console.WriteLine("Product Added!");
-            }
-            public void Update()
-            {
-                Console.WriteLine("Product Updated!");
-            }
-        }
+        
     }
 }
