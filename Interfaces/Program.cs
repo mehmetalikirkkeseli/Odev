0f2db8e57915e9c8e88c4161a14bdc591655cea8
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-10-25 19:43:52 +0300

    commit1

---
+++ Interfaces/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class Program
+    {
+        //Interface kavramı
+        //: ile interface class'a tanımlanır.
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    interface IPerson
+    {
+        int Id { get; set; }
+        int FirstName { get; set; }
+        int LastName { get; set; }
+    }
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+    }
+    class Student : IPerson
+    {
+        public int Id { get; set; }
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+    }
+}
