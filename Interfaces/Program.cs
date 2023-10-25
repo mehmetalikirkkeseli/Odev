46fb3959dbf7095bd5af300b7b2ee93d7635cb6f
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-10-25 19:52:38 +0300

    commit3

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -12,29 +12,39 @@ namespace Interfaces
         //: ile interface class'a tanımlanır.
         static void Main(string[] args)
         {
+            PersonManager manager = new PersonManager();
+            manager.Add(new Customer { Id = 1, FirstName = "Engin", LastName = "Demiroğ", Address = "Ankara" });
 
+            Console.ReadLine();
         }
     }
     interface IPerson
     {
         int Id { get; set; }
-        int FirstName { get; set; }
-        int LastName { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
     }
     class Customer : IPerson
     {
         public int Id { get; set; }
-        public int FirstName { get; set; }
-        public int LastName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
 
         public string Address { get; set; }
     }
     class Student : IPerson
     {
         public int Id { get; set; }
-        public int FirstName { get; set; }
-        public int LastName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
 
         public string Department { get; set; }
+    }
+    class PersonManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName);
+        }
     }
 }
