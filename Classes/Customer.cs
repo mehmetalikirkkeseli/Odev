7ea473a9fa641d29fdda7a13ad181b0e0f71d1ba
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-10-25 17:49:49 +0300

    commit6

--- Classes/Customer.cs
+++ Classes/Customer.cs
@@ -8,8 +8,9 @@ namespace Classes
 {
     internal class Customer
     {   //Property eklemek için prop kısayolu kullanılır.
+        public string FirstName;
         public int Id { get; set; }
-        public string FirstName { get; set; }
+        //public string FirstName { get; set; }
         public string LastName { get; set; }
         public string City { get; set; }
     }
