4e77029afbf8f6340a92aa082b706a03aa116d4e
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-10-25 16:30:36 +0300

    commit1

---
+++ Classes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Program
+    {
+        //Classlar
+        static void Main(string[] args)
+        {
+            //Class çağırma
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            Console.ReadLine();
+        }
+
+        class CustomerManager
+        {
+            public void Add()
+            {
+                Console.WriteLine("Customer Added!");
+            }
+            public void Update()
+            {
+                Console.WriteLine("Custoemer Updated!");
+            }
+        }
+    }
+}
