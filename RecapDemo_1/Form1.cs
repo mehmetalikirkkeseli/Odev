195773f2f31c3612dbe0a0747eefe916d0aebe93
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-10-25 19:03:33 +0300

    commit6

--- RecapDemo_1/Form1.cs
+++ RecapDemo_1/Form1.cs
@@ -8,11 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace RecapDemo1
+namespace RecapDemo_1
 {
     public partial class Form1 : Form
     {
-        //Windows Forms uygulaması açılmıştır.
         public Form1()
         {
             InitializeComponent();
@@ -21,7 +20,9 @@ namespace RecapDemo1
         private void Form1_Load(object sender, EventArgs e)
         {
             //64 tane buton için dizi oluşturulmuştur.
-            Button[,] buttons = new Button[8,8];
+            Button[,] buttons = new Button[8, 8];
+            int top = 0;
+            int left = 0;
             for (int i = 0; i < buttons.GetUpperBound(0); i++)
             {
                 for (int j = 0; j < buttons.GetUpperBound(1); j++)
@@ -29,12 +30,12 @@ namespace RecapDemo1
                     buttons[i, j] = new Button();
                     buttons[i, j].Width = 50;
                     buttons[i, j].Height = 50;
+                    buttons[i, j].Left = left;
+                    left += 50;
                     //Butonu ekrana ekleme
-                    this.Controls.Add(buttons[i,j]);
+                    this.Controls.Add(buttons[i, j]);
                 }
             }
-
-
         }
     }
 }
