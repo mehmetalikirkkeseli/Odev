055d308d8f13b3c29f847440ba7213105c884f88
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-10-26 14:15:08 +0300

    commit2

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -11,8 +11,26 @@ namespace Inheritance
         //Inheritance(Kalıtım)
         static void Main(string[] args)
         {
-            Customer customer = new Customer();
-            
+            Person[] persons = new Person[3]
+            {
+                new Customer
+                {
+                    FirstName = "Engin"
+                },
+                new Student
+                {
+                    FirstName = "Derin"
+                },
+                new Person
+                {
+                    FirstName = "Salih"
+                }
+            };
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+            Console.ReadLine();
         }
     }
     class Person
@@ -21,8 +39,12 @@ namespace Inheritance
         public string FirstName { get; set; }
         public string LastName { get; set; }
     }
-    class Customer:Person
+    class Customer : Person
     {
         public string City { get; set; }
+    }
+    class Student : Person
+    {
+        public string Department { get; set; }
     }
 }
