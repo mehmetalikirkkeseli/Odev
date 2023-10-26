6af35a2716f9c40e6ee5b2cdcc5f5a1c353e3f8d
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-10-26 13:46:43 +0300

    commit1

---
+++ InterfacesDemo/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesDemo
+{
+    internal class Program
+    {
+        //Çoklu implementasyon
+        static void Main(string[] args)
+        {
+        }
+    }
+    //IWorker interface'i oluşturulmuştur.
+    interface IWorker
+    {
+        void Work();
+        void Eat();
+        void GetSalary();
+            
+    }
+    class Manager : IWorker
+    {
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Worker : IWorker
+    {
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Robot : IWorker
+    {
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
