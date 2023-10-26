d0829f90e01f4d92cd06d0ac6a696b4ced297387
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-10-26 14:08:35 +0300

    commit1

---
+++ Inheritance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        //Inheritance(Kalıtım)
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            
+        }
+    }
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    class Customer:Person
+    {
+        public string City { get; set; }
+    }
+}
