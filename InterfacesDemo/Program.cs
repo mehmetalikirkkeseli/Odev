1693880dfaba3556080457d35d7fc9125402327f
Mehmet Ali Kırkkeseli <mehmet.alik2012@hotmail.com>
2023-10-26 14:02:51 +0300

    commit4

--- InterfacesDemo/Program.cs
+++ InterfacesDemo/Program.cs
@@ -11,6 +11,27 @@ namespace InterfacesDemo
         //Çoklu implementasyon
         static void Main(string[] args)
         {
+            //IWorker interface'i çağrılmıştır.
+            IWorker[] workers = new IWorker[3]
+            {
+                new Manager(),
+                new Worker(),
+                new Robot()
+            };
+            foreach (var worker in workers)
+            {
+                worker.Work();
+            }
+            //IEat interface'i çağrılmıştır.
+            IEat[] eats = new IEat[2]
+            {
+                new Worker(),
+                new Manager()
+            };
+            foreach (var eat in eats)
+            {
+                eat.Eat();
+            }
         }
     }
     //IWorker interface'i oluşturulmuştur.
@@ -33,41 +54,41 @@ namespace InterfacesDemo
     {
         public void GetSalary()
         {
-            throw new NotImplementedException();
+            
         }
 
         public void Work()
         {
-            throw new NotImplementedException();
+            
         }
 
         void IEat.Eat()
         {
-            throw new NotImplementedException();
+            
         }
     }
     class Worker : IWorker, IEat, ISalary
     {
         public void Eat()
         {
-            throw new NotImplementedException();
+            
         }
 
         public void GetSalary()
         {
-            throw new NotImplementedException();
+            
         }
 
         public void Work()
         {
-            throw new NotImplementedException();
+            
         }
     }
     class Robot : IWorker
     {
         public void Work()
         {
-            throw new NotImplementedException();
+            
         }
     }
 }
